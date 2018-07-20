e78f68c68de0e7f1c2f246311c7e3a7b31852cae
Erika Pauwels <erika.pauwels@gmail.com>
2018-07-20 20:15:41 +0200

    Preload options in dynamic-select component

--- addon/components/input-fields/dynamic-select/edit.js
+++ addon/components/input-fields/dynamic-select/edit.js
@@ -2,6 +2,7 @@ import { debug } from '@ember/debug';
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { task, timeout } from 'ember-concurrency';
+import { alias } from '@ember/object/computed';
 import layout from '../../../templates/components/input-fields/dynamic-select/edit';
 
 export default Component.extend({
@@ -12,12 +13,31 @@ export default Component.extend({
   allowClear: true,
   disabled: false,
 
-  buildQueryParams(options){
-    return ( searchString ) => {
-      const filter = options.filter.queryParams;
-      filter[options.filter.filterKey] = searchString;
-      return filter;
-    };
+  displayProperty: alias('model.options.displayProperty'),
+  queryModel: alias('model.options.queryModel'),
+
+  async init() {
+    this._super(...arguments);
+
+    const options = await this.search.perform();
+    this.set('initialOptions', options);
+  },
+
+  async didReceiveAttrs(){
+    this._super(...arguments);
+
+    if (this.get('model')) {
+      let value = await this.get(`solution.${this.get('model.identifier')}`);
+
+      if (value && value.get('isNew')) { // only already existing options are allowed
+        debug(`Reset value of dynamic-select '${this.queryModel}' to null`);
+        value.destroyRecord();
+        value = null;
+        this.set(`solution.${this.get('model.identifier')}`, value);
+      }
+
+      this.set('object_instance', value);
+    }
   },
 
   /**
@@ -34,33 +54,18 @@ export default Component.extend({
    *           }
    *   }
    */
-  async didReceiveAttrs(){
-    this._super(...arguments);
-    let options = this.get('model.options');
-    this.set('queryModel', options.queryModel);
-    this.set('displayProperty', options.displayProperty);
-    this.set('queryParams', this.buildQueryParams(options));
-
-    if (this.get('model')) {
-      let value = await this.get(`solution.${this.get('model.identifier')}`);
-
-      if (value && value.get('isNew')) { // only already existing options are allowed
-        debug(`Reset value of dynamic-select '${this.queryModel}' to null`);
-        value.destroyRecord();
-        value = null;
-        this.set(`solution.${this.get('model.identifier')}`, value);
-      }
-
-      this.set('object_instance', value);
-    }
-  },
-
   search: task(function* (searchData){
-    yield timeout(300);
-    const queryParams = this.get('queryParams')(searchData);
-    const resources = yield this.get('store').query(this.get('queryModel'), queryParams);
+    if (searchData)
+      yield timeout(300);
+
+    const queryParams = this.get('model.options.filter.queryParams');
+    queryParams['page'] = { size: 200 };
+    if (searchData)
+      queryParams[this.get('model.options.filter.filterKey')] = searchData;
+
+    const resources = yield this.get('store').query(this.queryModel, queryParams);
     return resources;
-  }),
+  }).keepLatest(),
 
   actions: {
     select(object_instance){
