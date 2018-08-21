44681b8443c4e6246bf5f634478b0d71dedbd508
Aad Versteden <madnificent@gmail.com>
2018-08-21 12:43:27 +0200

    WIP

--- addon/components/edit-child.js
+++ addon/components/edit-child.js
@@ -8,5 +8,15 @@ export default Component.extend(IsRegisteredChild, ChildNode, {
   layout,
   childComponentName: computed('model.displayType', function() {
     return `input-fields/${this.get('model.displayType')}/edit`;
-  })
+  }),
+  
+  actions: {
+    updateValue(value) {
+      if( this.solution && this.get('model.identifier') ) {
+        this.set(`solution.${this.get('model.identifier')}`, value);
+      } else {
+        console.log("Tried to set value before solution or identifier was known");
+      }
+    }
+  }
 });
--- addon/components/input-fields/checkbox/edit.js
+++ addon/components/input-fields/checkbox/edit.js
@@ -3,17 +3,10 @@ import layout from '../../../templates/components/input-fields/checkbox/edit';
 
 export default Component.extend({
   layout,
-  didReceiveAttrs() {
-    this._super(...arguments);
-    if (this.get('model')) {
-      const value = this.get(`solution.${this.get('model.identifier')}`);
-      this.set('value', value);
-    }
-  },
+  
   actions: {
-    editSolution() {
-      const prop = this.get('model.identifier');
-      this.set(`solution.${prop}`, !this.get('value'));
+    toggleValue() {
+      this.updateValue( !this.get('value') );
     }
   }
 });
