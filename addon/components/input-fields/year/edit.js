c43117271fe3fe590ba73646f66bc08b2c2c9a3f
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-08-28 10:18:10 +0200

    year input: use internalValue. no render twice

--- addon/components/input-fields/year/edit.js
+++ addon/components/input-fields/year/edit.js
@@ -1,7 +1,15 @@
+import { oneWay } from '@ember/object/computed';
 import Component from '@ember/component';
 import layout from '../../../templates/components/input-fields/year/edit';
 import InputField from '../../../mixins/input-field';
 
 export default Component.extend( InputField, {
-  layout
+  layout,
+  internalValue: oneWay('value'),
+
+  actions: {
+    update() {
+      this.updateValue( this.internalValue );
+    }
+  }
 });
--- addon/components/input-fields/year/show.js
+++ addon/components/input-fields/year/show.js
@@ -1,14 +1,9 @@
+import { oneWay } from '@ember/object/computed';
 import Component from '@ember/component';
 import layout from '../../../templates/components/input-fields/year/show';
 import InputField from '../../../mixins/input-field';
 
 export default Component.extend( InputField, {
   layout,
-  didReceiveAttrs() {
-    this._super(...arguments);
-    if (this.get('model')) {
-      const value = this.get(`solution.${this.get('model.identifier')}`);
-      this.set('value', value);
-    }
-  }
+  internalValue: oneWay('value')
 });
